c7afc951163aede31115534addcef3491dd9d13a
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-01-10 23:26:54 +0300

    fire comit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+import 'package:get/get.dart';
+Future<void> main () async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      InitialRoute: AppPages.INTIAL,
+      home: Scaffold(
+        body: Center(
+          child: Text('working '),
+        ),
+      ),
+    );
+  }
+}
