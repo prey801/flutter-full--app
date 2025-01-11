b6498a70a36700c0eb4074c426d8ac2a79c839c1
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-01-11 23:15:56 +0300

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,22 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase/screens/welcome_screen.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart'; 
 
-import 'firebase_options.dart';
-import 'package:get/get.dart';
-Future<void> main () async {
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
-  runApp(const MyApp());
+void main() async { 
+  //WidgetsFlutterBinding.ensureInitialized(); 
+  //await Firebase.initializeApp(); 
+  runApp(const MyApp()); 
 }
 
+class MyApp extends StatelessWidget { 
+  const MyApp({super.key}); 
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      InitialRoute: AppPages.INTIAL,
-      home: Scaffold(
-        body: Center(
-          child: Text('working '),
-        ),
-      ),
-    );
-  }
+  @override 
+  Widget build(BuildContext context) { 
+    return const MaterialApp( 
+      debugShowCheckedModeBanner: false, 
+      home: WelcomeScreen(), 
+      title: "flutterPhone",
+    ); 
+  } 
 }
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: /* Add your content here, e.g., a Text widget */
+              Text('Welcome!', style: TextStyle(fontSize: 24)), 
+          ),
+        ),
+      ),
+    );
+  }
+}
